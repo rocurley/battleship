67c4f93361a94eddd0ccaf2781f223d3178b8f8b
Roger Curley <rocurley@gmail.com>
2017-10-10 15:42:50 -0700

    More tests, fewer lenses

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,22 +3,28 @@
 module Lib where
 
 import qualified Data.Array as Array
-import Data.Array (Array, (//))
+import Data.Array (Array, (//), (!))
 
 import qualified Data.Map as Map
 import Data.Map (Map)
 
 import Data.List
 
+import Safe
+
 import Control.Lens
 
+import Control.Monad
 import Control.Monad.State
+import Control.Monad.Trans.Maybe
+
+import Control.Monad.Random
 
 data Orientation = Vertical | Horizontal
 
 newtype ShipKey = ShipKey (Int, Int) deriving (Show, Eq, Ord)
 newtype ShipData = ShipData { _health :: Int
-                         }
+                            } deriving (Show, Eq, Ord)
 makeLenses ''ShipData
 type Ship = (ShipKey, ShipData)
 
@@ -26,7 +32,7 @@ data BeenHit = WasHit | NotHit deriving (Show, Eq, Ord)
 
 data GridCell = GridCell { _beenHit :: BeenHit
                          , _ship :: Maybe ShipKey
-                         }
+                         } deriving (Eq, Ord)
 makeLenses ''GridCell
 instance Show GridCell where
     show (GridCell NotHit Nothing) = "~"
@@ -46,7 +52,7 @@ showGrid grid = intercalate "\n"
 
 data GameState = GameState { _grid :: Grid
                            , _ships :: Map ShipKey ShipData
-                           }
+                           } deriving (Eq, Ord)
 makeLenses ''GameState
 
 instance Show GameState where
@@ -54,29 +60,65 @@ instance Show GameState where
     
 emptyState (h,w) = GameState (Array.listArray ((0,0),(h-1,w-1)) (repeat emptyGridCell)) Map.empty
 
---Does no checking that the target is valid or clear!
-insertShip :: ShipKey -> Orientation -> Int -> GameState -> GameState
-insertShip key@(ShipKey (y,x)) orientation length = execState $ do
-    ships.at key.= Just (ShipData length)
-    let gridUpdate = case orientation of
-                    Vertical   -> [((y',x), GridCell NotHit $ Just key) | y' <- [y..y+length-1]]
-                    Horizontal -> [((y,x'), GridCell NotHit $ Just key) | x' <- [x..x+length-1]]
-    grid %= (// gridUpdate)
+arrayGet :: (Array.Ix i) => i -> Array i a -> Maybe a
+arrayGet i arr
+  | Array.inRange (Array.bounds arr) i = Just $ arr ! i
+  | otherwise = Nothing
+
+arraySet :: (Array.Ix i) => i -> a -> Array i a -> Maybe (Array i a)
+arraySet i a arr = arr // [(i, a)] <$ arrayGet i arr
+
+addShipToCell :: ShipKey -> Grid -> (Int, Int) -> Maybe Grid
+addShipToCell key grid (y,x) = do
+  --Pattern match failure gives a nothing and protects from ships already there
+  GridCell beenHit Nothing <- arrayGet (y,x) grid
+  arraySet (y,x) (GridCell beenHit $ Just key) grid
+
+insertShip :: ShipKey -> Orientation -> Int -> GameState -> Maybe GameState
+insertShip key@(ShipKey (y,x)) orientation length (GameState grid ships) = let
+      newShips = Map.insert key (ShipData length) ships
+      shipCells = case orientation of
+                      Vertical   -> [(y',x) | y' <- [y..y+length-1]]
+                      Horizontal -> [(y,x') | x' <- [x..x+length-1]]
+      newGrid = foldM (addShipToCell key) grid shipCells
+   in (\ g -> GameState g newShips) <$> newGrid
 
 data ShotOutcome = Hit
                  | Miss
                  | AlreadyTaken
+                 | OffGrid
                  | Sunk
                  | Win
                  deriving (Show, Eq, Ord)
 
-isLost :: GameState -> Bool
-isLost (GameState _ ships) = all (\ ship -> ship^.health == 0) ships
+isLost :: Map ShipKey ShipData -> Bool
+isLost = all (\ ship -> ship^.health == 0) 
 
-fire :: (Int, Int) -> State GameState ShotOutcome
-fire pos =
+fire :: (Int, Int) -> GameState -> (ShotOutcome, GameState)
+fire pos gameState@(GameState grid ships) =
+  case arrayGet pos grid of
+    Nothing -> (OffGrid, gameState)
+    Just (GridCell WasHit _) -> (AlreadyTaken, gameState)
+    Just (GridCell NotHit Nothing) -> let
+      newGrid = fromJustNote "pos stopped being valid halfway through" $
+        arraySet pos (GridCell WasHit Nothing) grid
+     in (Miss, GameState newGrid ships)
+    Just (GridCell NotHit (Just shipKey)) -> let
+      newGrid = fromJustNote "pos stopped being valid halfway through" $
+        arraySet pos (GridCell WasHit $ Just shipKey) grid
+      newShips = Map.adjust (\ (ShipData health) -> ShipData $ health - 1) shipKey ships
+      outcome = case Map.lookup shipKey newShips of
+                  Nothing -> error "Hit ship not in ships"
+                  Just (ShipData 0) -> if isLost newShips
+                               then Win
+                               else Sunk
+                  _ -> Hit
+      in (outcome, GameState newGrid newShips)
+
+oldFire :: (Int, Int) -> State GameState ShotOutcome
+oldFire pos =
     preuse (grid.ix pos) >>= \case
-        Nothing -> error "Fired off grid"
+        Nothing -> return OffGrid
         Just (GridCell WasHit _) -> return AlreadyTaken
         Just (GridCell NotHit Nothing) -> do
             grid.ix pos.beenHit.= WasHit
@@ -86,8 +128,34 @@ fire pos =
             ships.ix shipKey.health -= 1
             preuse (ships.ix shipKey.health) >>= \case
                 Nothing -> error "Hit ship not in ships"
-                Just 0 -> isLost <$> get >>= \case
+                Just 0 -> isLost <$> use ships >>= \case
                     True -> return Win
                     False -> return Sunk
                 _ -> return Hit
+
+--Not guarenteed to terminate.
+--Given a way to lazily generate a permutation we could fix that, but 
+--for now attempting to add a ship if it won't fit is an error.
+addRandomShip :: MonadRandom m => Int -> GameState -> m GameState
+addRandomShip l initialState@(GameState grid ships) = let
+  --Not height and width (!): haskell array bounds are inclusive.
+  ((0,0),(gridMaxY,gridMaxX)) = Array.bounds grid
+  (h,w) = (gridMaxY + 1, gridMaxX + 1)
+  randOrientation =  case (h >= l, w >= l) of
+                     (True, True) -> (\ b -> if b then Horizontal else Vertical) <$> getRandom
+                     (True, False) -> return Vertical
+                     (False, True) -> return Horizontal
+                     (False, False) -> error "Ship doesn't fit"
+  go = do
+    orientation <- randOrientation
+    pos <- case orientation of
+               Horizontal -> (,) <$> getRandomR (0, h-1) <*> getRandomR (0, w-l)
+               Vertical   -> (,) <$> getRandomR (0, h-l) <*> getRandomR (0, w-1)
+    let newState = insertShip (ShipKey pos) orientation l  initialState
+    maybe go return newState
+  in go
+
+stateWithShips ::MonadRandom m => (Int, Int) -> m GameState
+stateWithShips dims = foldM (flip addRandomShip) (emptyState dims) [4,3,3]
+
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,20 +1,121 @@
+{-# LANGUAGE LambdaCase #-}
+
 import Lib
 
 import Control.Monad.State
 
-import Test.HUnit hiding (State)
+import qualified Data.Map as Map
+import Data.Map (Map)
+
+import Safe
+
+import Data.Maybe
+import Data.Foldable
+import Data.List
+
+import Control.Monad.Random
+
+import Test.Tasty
+import Test.Tasty.HUnit
 import Test.Hspec.Expectations
+import Test.Tasty.QuickCheck as QC
+import Test.QuickCheck.Random
 
-main = runTestTT $ TestList [TestCase shotHistoryTest, TestCase missfireTest]
+main :: IO ()
+main = defaultMain tests
 
-shotHistoryTest = let
+tests :: TestTree
+tests = testGroup "Tests" [unitTests, propertyTests]
+
+propertyTests :: TestTree
+propertyTests = testGroup "QuickCheck Tests" [singleRandomShipTest, stateWithShipsTest, randomFiringTest]
+
+unitTests :: TestTree
+unitTests = testGroup "Unit Tests" [shotHistoryTest, oldShotHistoryTest]
+
+counts :: (Ord a, Foldable t) => t a -> Map a Int
+counts = foldl' (\ acc k -> Map.insertWith (+) k 1 acc) Map.empty
+
+shipLengthsMatch :: [Int] -> GameState -> Bool
+shipLengthsMatch lengths (GameState grid ships) = let
+  cellCounts = counts $ mapMaybe (\ (GridCell _ maybeShipKey) -> maybeShipKey) $ toList grid
+  shipHealths = (\ (ShipData health) -> health) <$> ships
+  in cellCounts == shipHealths && counts shipHealths == counts lengths
+
+singleRandomShipTest :: TestTree
+singleRandomShipTest = QC.testProperty "Randomly adding a ship to an empty board always works" $
+  \ (QCGen gen) (Positive h, Positive w) (Positive l) -> (h >= l || w >= l) ==> let
+    state = evalRand (addRandomShip l $ emptyState (h,w)) gen
+ in shipLengthsMatch [l] state
+
+stateWithShipsTest  :: TestTree
+stateWithShipsTest = QC.testProperty "stateWithShips has 2 length 3 ships and 1 length 4 ship" $
+  \ (QCGen gen) (h,w) -> h >= 4 && w >=4 ==> shipLengthsMatch [3,3,4] $ evalRand (stateWithShips (h, w)) gen
+
+countHits :: GameState -> Int
+countHits (GameState grid _) = length $ filter (\case
+                                                GridCell WasHit _ -> True 
+                                                GridCell NotHit _ -> False) $ toList grid
+
+totalHealth :: GameState -> Int
+totalHealth (GameState _ ships) = sum $ (\ (ShipData health) -> health) <$> ships
+
+validateShotOutcome :: GameState -> ShotOutcome -> GameState -> Bool
+validateShotOutcome initial AlreadyTaken final = initial == final
+validateShotOutcome initial OffGrid final = initial == final
+validateShotOutcome initial outcome final = let
+  nHits = case outcome of
+            Hit -> 1
+            Sunk -> 1
+            Win -> 1
+            Miss -> 1
+            AlreadyTaken -> 0
+            OffGrid -> 0
+  dHealth = case outcome of
+              Hit -> -1
+              Sunk -> -1
+              Win -> -1
+              Miss -> 0
+              AlreadyTaken -> 0
+              OffGrid -> 0
+  in (countHits initial + nHits == countHits final)
+     && (totalHealth initial + dHealth == totalHealth final)
+
+shotsHistory :: GameState -> [(Int, Int)] -> [(GameState, ShotOutcome, GameState)]
+shotsHistory initialState targets = unfoldr
+  (\case 
+    (_, []) -> Nothing
+    (state, target:targets) -> let
+      (shotOutcome, newState) = fire target state 
+      in Just ((state, shotOutcome, newState), (newState, targets))
+  ) (initialState, targets)
+
+--Note that these can (deliberately) fire off-grid
+shotsGen :: (Int, Int) -> Gen [(Int, Int)]
+shotsGen (h,w) = replicateM (h*w) $ (,) <$> choose (0, h) <*> choose (0, w)
+
+randomFiringTest  :: TestTree
+randomFiringTest = QC.testProperty "random shots yield reasonable results" $
+  \ (QCGen gen) (h,w) -> h >= 4 && w >=4 ==> forAll (shotsGen (h,w)) $ \ targets -> let
+    initialState = evalRand (stateWithShips (h, w)) gen
+    log = shotsHistory initialState targets
+ in all (\(state, outcome, finalState) -> validateShotOutcome state outcome finalState) log
+
+
+oldShotHistoryTest :: TestTree
+oldShotHistoryTest = testCase "Pre-prepared sequence of shots (Old firing function)" $ let
   log = (`evalState` emptyState (10,10)) $ do
-          modify $ insertShip (ShipKey (0,0)) Horizontal 3
-          modify $ insertShip (ShipKey (3,3)) Vertical 2
-          traverse fire [(0,0), (0,1), (0,0), (5,5), (5,5), (0,2), (3,3), (4,3)]
-       in log `shouldBe` [Hit, Hit, AlreadyTaken, Miss, AlreadyTaken, Sunk, Hit, Win]
-
-missfireTest = do
-  print (fire (10,5) `evalState` emptyState (10,10)) `shouldThrow` errorCall "Fired off grid"
-  print (fire (0,-1) `evalState` emptyState (10,10)) `shouldThrow` errorCall "Fired off grid"
+          modify $ fromJustNote "first ship" . insertShip (ShipKey (0,0)) Horizontal 3
+          modify $ fromJustNote "seccond ship" . insertShip (ShipKey (3,3)) Vertical 2
+          traverse oldFire [(0,0), (10,5), (0,1), (0,0), (5,5), (5,5), (0,2), (3,3), (0,-1), (4,3)]
+       in log `shouldBe` [Hit, OffGrid, Hit, AlreadyTaken, Miss, AlreadyTaken, Sunk, Hit, OffGrid, Win]
   
+shotHistoryTest :: TestTree
+shotHistoryTest = testCase "Pre-prepared sequence of shots" $ let
+  initialState =
+    fromJustNote "seccond ship" $ insertShip (ShipKey (3,3)) Vertical 2 $
+      fromJustNote "first ship" $ insertShip (ShipKey (0,0)) Horizontal 3 $
+      emptyState (10,10)
+  targets = [(0,0), (10,5), (0,1), (0,0), (5,5), (5,5), (0,2), (3,3), (0,-1), (4,3)]
+  log = (\ (_,outcome,_) -> outcome) <$> shotsHistory initialState targets
+  in log `shouldBe` [Hit, OffGrid, Hit, AlreadyTaken, Miss, AlreadyTaken, Sunk, Hit, OffGrid, Win]
